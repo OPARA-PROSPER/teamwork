3c6aeae5233dee6a3dfc333e314b93608a6c6631
Opara Prosper <oparaprosper79@gmail.com>
2019-11-17 00:10:56 +0100

    fix no response from server error, add better error handling

--- api/middlewares/auth.js
+++ api/middlewares/auth.js
@@ -2,22 +2,30 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")[1];
-    const verifyToken = jwt.verify(token, "TEAMWORK_SECRET_KEY");
-    const userID = verifyToken.userID;
+    const token = req.headers.authorization;
 
-    if (req.body.id && req.body.id !== userID) {
-      res.status(400).json({
+    if (token) {
+      const verifyToken = jwt.verify(token, 'TEAMWORK_SECRET_KEY');
+      const { userEmail } = verifyToken;
+
+      if (req.body.email && req.body.email !== userEmail) {
+        res.status(400).json({
+          status: 'error',
+          error: 'Invalid user email',
+        });
+      } else {
+        next();
+      }
+    } else {
+      res.status(403).json({
         status: 'error',
-        error: "Invalid user ID",
-      })
-    }else{
-      next();
+        error: 'No authentication token supplied',
+      });
     }
-  } catch {
+  } catch (err) {
     res.status(401).json({
       status: 'error',
-      error: 'You are authorized'
+      error: `You are authorized: ${err}`,
     });
   }
 };
