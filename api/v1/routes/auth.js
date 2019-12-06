1f070a1320bd595cf7140792c3e2206df3ff8a60
Opara Prosper <oparaprosper79@gmail.com>
2019-12-06 05:25:00 +0100

    Refactor to use new controller name

--- api/v1/routes/auth.js
+++ api/v1/routes/auth.js
@@ -7,13 +7,13 @@ const adminAuth = require('../middlewares/adminAuth');
 const checkToken = require('../middlewares/tokenCheck');
 
 // load controllers
-const authUserController = require('../controllers/authUserController');
+const UsersController = require('../controllers/UsersController');
 const gifsController = require('../controllers/gifsContoller');
 const articlesController = require('../controllers/articlesController');
 
-router.get('/auth', adminAuth, authUserController.getUsers);
-router.post('/auth/create-user', adminAuth, authUserController.createUSer);
-router.post('/auth/signin', authUserController.signIn);
+router.get('/auth', adminAuth, UsersController.getUsers);
+router.post('/auth/create-user', adminAuth, UsersController.createUSer);
+router.post('/auth/signin', UsersController.signIn);
 
 router.post('/articles', articlesController.postArticles);
 router.patch('/articles/:id', checkToken, articlesController.patchArticle);
