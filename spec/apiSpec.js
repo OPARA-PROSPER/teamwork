32719f20e744478adfea70a5c2fecf0e4c83b755
Opara Prosper <oparaprosper79@gmail.com>
2019-12-06 22:24:24 +0100

    Experiment adding server status test

--- spec/apiSpec.js
+++ spec/apiSpec.js
@@ -4,6 +4,18 @@ const request = require('request');
 
 describe('Api', () => {
   const url = 'http://localhost:3000/api/v1/auth';
+  // let server;
+  //
+  // describe('Check that server is up', () => {
+  // beforeAll((done) => {
+  // server = require('../server');
+  // done();
+  // });
+  //
+  // it('and running', () => {
+  // expect(server).toBe(0);
+  // });
+  // });
 
   describe('client request to get all users', () => {
     it('without an admin token should return status code of 400', (done) => {
